88a109ea588be0e7b4d05d14e8af3ed8458f2831
Mpflueger15 <53787076+Mpflueger15@users.noreply.github.com>
2023-08-29 00:02:05 -0400

    add and edit frames

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -17,8 +17,37 @@ const App = () => {
   const [gamma, setGamma] = useState(1);
   const [RGBAdjustment, setRGBAdjustment] = useState([1, 1, 1]);
   const [drawing, setDrawing] = useState(false);
+  const [savedFrames, setSavedFrames] = useState<ImageData[]>([]);
+  const [selectedFrameIndex, setSelectedFrameIndex] = useState<number | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const socketRef = useRef<SocketIOClient.Socket>();
+  const deleteFrame = (index: number) => {
+    setSavedFrames(prevFrames => prevFrames.filter((_, idx) => idx !== index));
+  };
+
+  const saveCurrentFrame = () => {
+    const canvas = canvasRef.current!;
+    const ctx = canvas.getContext('2d')!;
+    const imageData = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    setSavedFrames(prevFrames => [...prevFrames, imageData]);
+  };
+
+  const displayFrame = (frameData: ImageData, index: number) => {
+    const canvas = canvasRef.current!;
+    const ctx = canvas.getContext('2d')!;
+    ctx.putImageData(frameData, 0, 0);
+    setSelectedFrameIndex(index)
+};
+
+  const updateSelectedFrame = () => {
+    if (selectedFrameIndex !== null) {
+        const canvas = canvasRef.current!;
+        const ctx = canvas.getContext('2d')!;
+        const imageData = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        setSavedFrames(prevFrames => prevFrames.map((frame, idx) => idx === selectedFrameIndex ? imageData : frame));
+        setSelectedFrameIndex(null); // Reset the selected frame index
+    }
+  };
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files![0];
@@ -190,93 +219,120 @@ const App = () => {
   };
 
   return (
-    <div>
-      <SketchPicker color={color} onChange={handleColorChange} />
-      <div style={{display: "grid"}}>
-        <label>
-          Brightness:
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={brightness}
-            onChange={handleBrightnessChange}
-          />
-        </label>
-        <span style={{display:"inline-flex"}}>
-          <label>
-            Gamma Correction:
-            <input
-              type="range"
-              min="0.1"
-              max="3.0"
-              step="0.1"
-              value={gamma}
-              onChange={handleGammaChange}
-            />
+    <div className="app-container">
+      <div className="tool-content">
+        <div>
+          <SketchPicker color={color} onChange={handleColorChange} />
+          <div style={{display: "grid"}}>
+            <label>
+              Brightness:
+              <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                value={brightness}
+                onChange={handleBrightnessChange}
+              />
+            </label>
+            <span style={{display:"inline-flex"}}>
+              <label>
+                Gamma Correction:
+                <input
+                  type="range"
+                  min="0.1"
+                  max="3.0"
+                  step="0.1"
+                  value={gamma}
+                  onChange={handleGammaChange}
+                />
+              </label>
+              <p>{gamma}</p>
+            </span>
+            <span style={{display:"inline-flex"}}>
+              <label>
+                Red Correction:
+                <input
+                  type="range"
+                  min="0.1"
+                  max="2.0"
+                  step="0.01"
+                  value={RGBAdjustment[0]}
+                  onChange={handleRedChange}
+                />
+              </label>
+              <p>{RGBAdjustment[0]}</p>
+            </span>
+            <span style={{display:"inline-flex"}}>
+              <label>
+                Green Correction:
+                <input
+                  type="range"
+                  min="0.1"
+                  max="2.0"
+                  step="0.01"
+                  value={RGBAdjustment[1]}
+                  onChange={handleGreenChange}
+                />
+              </label>
+              <p>{RGBAdjustment[1]}</p>
+            </span>
+            <span style={{display:"inline-flex"}}>
+              <label>
+                Blue Correction:
+                <input
+                  type="range"
+                  min="0.1"
+                  max="2.0"
+                  step="0.01"
+                  value={RGBAdjustment[2]}
+                  onChange={handleBlueChange}
+                />
+              </label>
+              <p>{RGBAdjustment[2]}</p>
+            </span>
+          </div>
+          <label style={{fontSize: '12pt', padding: '2px', cursor: 'pointer', borderRadius: '2px', border: '1px solid black', backgroundColor: 'white' }}>
+            Upload Image:
+            <input style={{display: 'none'}} type="file" accept="image/png, image/jpeg" onChange={handleImageUpload} />
           </label>
-          <p>{gamma}</p>
-        </span>
-        <span style={{display:"inline-flex"}}>
-          <label>
-            Red Correction:
-            <input
-              type="range"
-              min="0.1"
-              max="2.0"
-              step="0.01"
-              value={RGBAdjustment[0]}
-              onChange={handleRedChange}
-            />
-          </label>
-          <p>{RGBAdjustment[0]}</p>
-        </span>
-        <span style={{display:"inline-flex"}}>
-          <label>
-            Green Correction:
-            <input
-              type="range"
-              min="0.1"
-              max="2.0"
-              step="0.01"
-              value={RGBAdjustment[1]}
-              onChange={handleGreenChange}
-            />
-          </label>
-          <p>{RGBAdjustment[1]}</p>
-        </span>
-        <span style={{display:"inline-flex"}}>
-          <label>
-            Blue Correction:
-            <input
-              type="range"
-              min="0.1"
-              max="2.0"
-              step="0.01"
-              value={RGBAdjustment[2]}
-              onChange={handleBlueChange}
-            />
-          </label>
-          <p>{RGBAdjustment[2]}</p>
-        </span>
+        </div>
       </div>
-      <label style={{fontSize: '12pt', padding: '2px', cursor: 'pointer', borderRadius: '2px', border: '1px solid black', backgroundColor: 'white' }}>
-        Upload Image:
-        <input style={{display: 'none'}} type="file" accept="image/png, image/jpeg" onChange={handleImageUpload} />
-      </label>
-      <div className="canvas-container">
-        <canvas 
-          id="drawingCanvas"
-          ref={canvasRef} 
-          onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-          style={{cursor: `url(${pencilImg}) 0 24, auto`}}
-          />
-        <canvas id="viewingCanvas" width="256" height="256"></canvas>
+      <div className="main-content">
+        <div className="canvas-container">
+          <canvas 
+            id="drawingCanvas"
+            ref={canvasRef} 
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+            style={{cursor: `url(${pencilImg}) 0 24, auto`}}
+            />
+        </div>
+        <div className="button-container">
+          <button onClick={saveCurrentFrame}>Add New Frame</button>
+          <button onClick={updateSelectedFrame}>Update Selected Frame</button>
+          <button onClick={sendE131Data} style={{display: 'flex',  justifyContent: 'center', cursor: 'pointer'}}>Send</button>
+        </div>
       </div>
-      <button onClick={sendE131Data} style={{display: 'flex',  justifyContent: 'center', cursor: 'pointer'}}>Send</button>
+      <div className="frame-container">
+          <h3>Frames</h3>
+          {savedFrames.map((frameData, index) => (
+              <div className={`frame-preview ${index === selectedFrameIndex ? 'selected-frame' : ''}`} key={index}>
+                  <div className="scaled-container">
+                    <canvas 
+                        width={CANVAS_WIDTH} 
+                        height={CANVAS_HEIGHT} 
+                        ref={el => el && el.getContext('2d').putImageData(frameData, 0, 0)}
+                    />
+                  </div>
+                  <div className="button-container">
+                    <button onClick={() => deleteFrame(index)}>X</button>
+                    <button onClick={() => displayFrame(frameData, index)}>edit</button>
+                  </div>
+              </div>
+          ))} 
+      </div>
     </div>
   );
 };
